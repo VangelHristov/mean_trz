a696278774d9b42684ead330634e66e680589ec8
vangel <vangel.hristov@yahoo.com>
2017-02-28 20:54:23 +0200

    remove the hardcoded data from AddCompanyController

--- app_client/ng-app/controllers/add-company.js
+++ app_client/ng-app/controllers/add-company.js
@@ -5,30 +5,18 @@
       .module('app')
       .controller('AddCompanyController', ['storage', 'dataContext', 'notification', '$location',
           function (storage, dataContext, notification, $location) {
-          let ctrl = this;
-          ctrl.data = {
-              name                : 'Първа фирма',
-              bulstat             : '123456786',
-              director            : 'Вангел Христов',
-              address             : {
-                  street    : 'улица Марица',
-                  city      : 'Своге',
-                  postalCode: '1234',
-                  country   : 'България'
-              },
-              pkpv                : '1',
-              mainEconomicActivity: 'бизнес',
-              user                : storage.getUserId()
-          };
-          ctrl.save = function () {
-              dataContext.company
-                         .save(ctrl.data)
-                         .$promise
-                         .then(result => {
-                             notification.success(result.message);
-                             $location.path('/companies');
-                         })
-                         .catch(error => notification.warning(error.data));
-          };
-      }]);
+              let ctrl = this;
+              ctrl.data = {};
+              ctrl.data.user = storage.getUserId();
+              ctrl.save = function () {
+                  dataContext.company
+                             .save(ctrl.data)
+                             .$promise
+                             .then(result => {
+                                 notification.success(result.message);
+                                 $location.path('/companies');
+                             })
+                             .catch(error => notification.warning(error.message));
+              };
+          }]);
 }());
