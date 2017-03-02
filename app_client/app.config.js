1f811a999cc9423aef9c234a8f41bca4fe930341
vangel <vangel.hristov@yahoo.com>
2017-03-02 19:40:33 +0200

    add titles for each route in the app.config.js file

--- app_client/app.config.js
+++ app_client/app.config.js
@@ -7,31 +7,37 @@
           $routeProvider
             .when('/about', {
                 templateUrl: 'views/about.html',
-                title:'ТРЗ'
+                data       : {pageTitle: 'ТРЗ'}
             })
             .when('/companies', {
                 authorization: true,
-                templateUrl: 'views/companies.html'
+                templateUrl  : 'views/companies.html',
+                data         : {pageTitle: 'Начало'}
             })
             .when('/companies/add-new', {
                 authorization: true,
-                templateUrl: 'views/add-new-company.html'
+                templateUrl  : 'views/add-new-company.html',
+                data         : {pageTitle: 'Добави фирма'}
             })
             .when('/companies/:companyId', {
                 authorization: true,
-                templateUrl: 'views/company-details.html'
+                templateUrl  : 'views/company-details.html',
+                data         : {pageTitle: 'Фирмени данни'}
             })
             .when('/companies/:companyId/dossiers/add-new', {
                 authorization: true,
-                templateUrl: 'views/add-new-dossier.html'
+                templateUrl  : 'views/add-new-dossier.html',
+                data         : {pageTitle: 'Добави досие'}
             })
             .when('/companies/:companyId/dossiers/:dossierId', {
                 authorization: true,
-                templateUrl: 'views/dossier-details.html'
+                templateUrl  : 'views/dossier-details.html',
+                data         : {pageTitle: 'Досие'}
             })
             .when('/companies/:companyId/dossiers/:dossierId/add-work-contract', {
                 authorization: true,
-                templateUrl: 'views/add-work-contract.html'
+                templateUrl  : 'views/add-work-contract.html',
+                data         : {pageTitle: 'Добави трудов договор'}
             })
             .otherwise('/about');
 
